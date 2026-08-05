ed784a4f2e8637a60812c1c35671617ebc6b4eee
takahashiumaru <umarmarufmutaqin@gmail.com>
2026-08-05 12:09:34 +0700

    chore(lint): allow unused variables prefixed with underscore

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
       "no-console": "off",
     },
   },
