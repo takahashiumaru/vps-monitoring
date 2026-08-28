926b90f1b097844fc5767833d5dd1c2b3f1f6cf6
takahashiumaru <umarmarufmutaqin@gmail.com>
2026-08-28 12:14:20 +0700

    chore(lint): ignore generated ios and data directories in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 module.exports = [
   {
+    ignores: ["ios/**", "data/**", "node_modules/**"],
     rules: {
       "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
       "no-console": "off",
